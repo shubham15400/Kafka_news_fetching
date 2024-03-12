f2db73a9125177f415b1ace4e35ed67bdf5c6804
shubham15400 <shubhampatel15400@gmail.com>
2024-03-12 12:40:07 -0400

    consumer changes

--- kafka_consume.py
+++ kafka_consume.py
@@ -11,35 +11,25 @@ consumer = KafkaConsumer('news-topic', bootstrap_servers=bootstrap_servers)
 # Define the CSV file path
 csv_file_path = 'output.csv'
 
+# Specify the keys you want to store in the CSV
+selected_keys = ['author', 'title', 'description', 'content', 'publishedAt']  # Column names to be extracted
+
 # Open the CSV file in write mode
 with open(csv_file_path, 'w', newline='') as csvfile:
     # Create a CSV writer object
     csv_writer = csv.writer(csvfile)
 
+    # Write header to the CSV file
+    csv_writer.writerow(selected_keys)
+
     # Poll for new messages
     for messages in consumer:
         message = json.loads(messages.value.decode('utf-8'))
 
-        # Extract and flatten the nested JSON structure
-        flattened_data = {}
-
-        def flatten(data, prefix=''):
-            for key, value in data.items():
-                new_key = f"{prefix}_{key}" if prefix else key
-                if isinstance(value, dict):
-                    flatten(value, new_key)
-                else:
-                    flattened_data[new_key] = value
-
-        flatten(message)
-
-        # Write header to the CSV file (if not already written)
-        if not csvfile.tell():
-            header = list(flattened_data.keys())
-            csv_writer.writerow(header)
+        # Extract selected key-value pairs
+        row = [message.get(key, '') for key in selected_keys]
 
         # Write the row to the CSV file
-        row = list(flattened_data.values())
         csv_writer.writerow(row)
 
         print(f"Received message: {message}")
