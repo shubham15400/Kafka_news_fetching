fd01f82bd60eb1fc1db7254047c281ebce65d519
shubham15400 <shubhampatel15400@gmail.com>
2024-03-13 18:24:49 -0400

    Final consumer commit

--- kafka_consume.py
+++ kafka_consume.py
@@ -1,6 +1,7 @@
 import json
 from kafka import KafkaConsumer
 from datetime import datetime
+import pandas as pd  # Import pandas library
 
 # Kafka broker configuration
 bootstrap_servers = 'localhost:9092'
@@ -8,36 +9,42 @@ bootstrap_servers = 'localhost:9092'
 # Create Kafka Consumer instance
 consumer = KafkaConsumer('news-topic', bootstrap_servers=bootstrap_servers)
 
-# Define the JSON file path
-json_file_path = 'output.json'
+# Define the keys you want to store in the DataFrame
+selected_keys = ['author', 'title', 'description', 'content', 'publishedAt']
 
-# Specify the keys you want to store in the JSON
-selected_keys = ['author', 'title', 'description', 'content', 'publishedAt']  # Keys to be extracted
+# Create an empty list to store DataFrames
+df_list = []
 
-# Open the JSON file in write mode
-with open(json_file_path, 'w') as jsonfile:
-    # Poll for new messages
-    for message in consumer:
-        message_data = json.loads(message.value.decode('utf-8'))
+# Poll for new messages
+for message in consumer:
+    message_data = json.loads(message.value.decode('utf-8'))
+    
+    published_at_str = message_data.get('publishedAt', '')
+    if published_at_str:
+        try:
+            published_at_dt = datetime.strptime(published_at_str, '%Y-%m-%dT%H:%M:%SZ')
+            message_data['publishedAt'] = published_at_dt.timestamp()
+        except ValueError as e:
+            print(f"Error parsing publishedAt: {e}")
+            # Optionally, you can handle the error or skip this message
+            continue
 
-        published_at_str = message_data.get('publishedAt', '')
-        if published_at_str:
-            try:
-                published_at_dt = datetime.strptime(published_at_str, '%Y-%m-%dT%H:%M:%SZ')
-                message_data['publishedAt'] = published_at_dt.timestamp()
-            except ValueError as e:
-                print(f"Error parsing publishedAt: {e}")
-                # Optionally, you can handle the error or skip this message
-                continue
+    # Extract selected key-value pairs
+    filtered_data = {key: message_data.get(key, '') for key in selected_keys}
+    print(filtered_data)
+    # Create a DataFrame from the filtered data
+    # Append DataFrame to df_list
+    df_list.append(filtered_data)
 
-        # Extract selected key-value pairs
-        filtered_data = {key: message_data.get(key, '') for key in selected_keys}
 
-        # Write the data to the JSON file
-        json.dump(filtered_data, jsonfile)
-        jsonfile.write('\n')  # Write a new line for each JSON object
-
-        print(f"Received message: {filtered_data}")
-
+    # print(f"Received message: {filtered_data}")
+    if not consumer.poll(timeout_ms=6000):
+        print('no messages available, stopping consumer')
+        break
+df = pd.DataFrame(df_list)
+df = df.replace({',':''}, regex=True)
+print(df)
+df.to_csv('output.csv', index=False)
 # Close consumer
 consumer.close()
+
